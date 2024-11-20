aa8c72cb017d3f52c8f93950f9ce3b63f4653eaa
v4por <ithuluwa@163.com>
2024-11-20 21:00:01 +0800

    1. 原domain工具变更为smartproxy

---
+++ smartproxy/src/main/java/burp/config/Settings.java
@@ -0,0 +1,6 @@
+package burp.config;
+
+public class Settings {
+
+    public static volatile Boolean ICP_BUTTON = true;
+}
---
+++ smartproxy/src/main/java/burp/config/Website.java
@@ -0,0 +1,40 @@
+package burp.config;
+
+import java.util.regex.Pattern;
+
+public enum Website {
+
+    ICP("/icpproject_query/api/icpAbbreviateInfo/queryByCondition", "hlwicpfwc\\.miit\\.gov\\.cn"),
+
+    XLB("/api\\.xiaolanben\\.com/xlb-gateway/blue-book/group/groupData","sou\\.xiaolanben\\.com");
+
+    private String uri;
+    private String host;
+
+    private Pattern uriPattern;
+
+    private Pattern hostPattern;
+
+    Website(String uri, String host) {
+        this.uri = uri;
+        this.host = host;
+        this.hostPattern = Pattern.compile("^Host:\\s(" + host + ")$");
+        this.uriPattern = Pattern.compile("^(GET|POST)\\s(" + uri + ")");
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String host() {
+        return host;
+    }
+
+    public Pattern getUriPattern() {
+        return uriPattern;
+    }
+
+    public Pattern getHostPattern() {
+        return hostPattern;
+    }
+}
